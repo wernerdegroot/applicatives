05ff74936cbd95796a8734a50cd1845ab4462ad3
Werner de Groot <werner@yoink.nl>
2022-05-25 18:33:05 +0200

    Introduce abstract base class for shared processor logic.

--- processor/src/main/java/nl/wernerdegroot/applicatives/processor/CovariantBuilderProcessor.java
+++ processor/src/main/java/nl/wernerdegroot/applicatives/processor/CovariantBuilderProcessor.java
@@ -3,18 +3,14 @@ package nl.wernerdegroot.applicatives.processor;
 import com.google.auto.service.AutoService;
 import nl.wernerdegroot.applicatives.processor.converters.ContainingClassConverter;
 import nl.wernerdegroot.applicatives.processor.converters.MethodConverter;
-import nl.wernerdegroot.applicatives.processor.domain.*;
+import nl.wernerdegroot.applicatives.processor.domain.FullyQualifiedName;
+import nl.wernerdegroot.applicatives.processor.domain.Method;
+import nl.wernerdegroot.applicatives.processor.domain.Modifier;
 import nl.wernerdegroot.applicatives.processor.domain.containing.ContainingClass;
-import nl.wernerdegroot.applicatives.processor.domain.type.Type;
-import nl.wernerdegroot.applicatives.processor.domain.typeconstructor.TypeConstructor;
 import nl.wernerdegroot.applicatives.processor.generator.ContainingClassGenerator;
 import nl.wernerdegroot.applicatives.processor.generator.ParameterGenerator;
 import nl.wernerdegroot.applicatives.processor.generator.TypeGenerator;
-import nl.wernerdegroot.applicatives.processor.generator.TypeParameterGenerator;
 import nl.wernerdegroot.applicatives.processor.logging.Log;
-import nl.wernerdegroot.applicatives.processor.logging.LoggingBackend;
-import nl.wernerdegroot.applicatives.processor.logging.MessagerLoggingBackend;
-import nl.wernerdegroot.applicatives.processor.logging.NoLoggingBackend;
 import nl.wernerdegroot.applicatives.processor.validation.TemplateClassWithMethods;
 import nl.wernerdegroot.applicatives.processor.validation.TemplateClassWithMethodsValidator;
 import nl.wernerdegroot.applicatives.processor.validation.Validated;
@@ -25,14 +21,7 @@ import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
-import javax.tools.Diagnostic;
-import javax.tools.JavaFileObject;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Stream;
 
@@ -40,15 +29,12 @@ import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toSet;
 import static javax.lang.model.element.ElementKind.METHOD;
 import static nl.wernerdegroot.applicatives.processor.Classes.*;
-import static nl.wernerdegroot.applicatives.processor.conflicts.ConflictFinder.findClassTypeParameterNameReplacements;
-import static nl.wernerdegroot.applicatives.processor.conflicts.Conflicts.*;
-import static nl.wernerdegroot.applicatives.processor.generator.Generator.generator;
 
 @SupportedOptions({Options.VERBOSE_ARGUMENT})
 @SupportedAnnotationTypes(COVARIANT_BUILDER_CANONICAL_NAME)
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 @AutoService(Processor.class)
-public class CovariantBuilderProcessor extends AbstractProcessor {
+public class CovariantBuilderProcessor extends AbstractCovariantProcessor {
 
     public static final Set<FullyQualifiedName> SUPPORTED_ANNOTATIONS = Stream.of(
             INITIALIZER,
@@ -115,67 +101,13 @@ public class CovariantBuilderProcessor extends AbstractProcessor {
                     }
 
                     TemplateClassWithMethods templateClassWithMethods = validatedTemplateClassWithMethods.getValue();
-                    Log.of("All criteria for code generation satisfied")
-                            .withDetail("Class type parameters", templateClassWithMethods.getClassTypeParameters(), TypeParameterGenerator::generateFrom)
-                            .withDetail("Name of initializer method", templateClassWithMethods.getOptionalInitializer().map(CovariantInitializer::getName))
-                            .withDetail("Initialized type constructor", templateClassWithMethods.getOptionalInitializer().map(CovariantInitializer::getInitializedTypeConstructor), this::typeConstructorToString)
-                            .withDetail("Name of accumulator method", templateClassWithMethods.getAccumulator().getName())
-                            .withDetail("Input type constructor", templateClassWithMethods.getAccumulator().getInputTypeConstructor(), this::typeConstructorToString)
-                            .withDetail("Partially accumulated type constructor", templateClassWithMethods.getAccumulator().getPartiallyAccumulatedTypeConstructor(), this::typeConstructorToString)
-                            .withDetail("Accumulated type constructor", templateClassWithMethods.getAccumulator().getAccumulatedTypeConstructor(), this::typeConstructorToString)
-                            .withDetail("Name of finalizer method", templateClassWithMethods.getOptionalFinalizer().map(CovariantFinalizer::getName))
-                            .withDetail("To finalize type constructor", templateClassWithMethods.getOptionalFinalizer().map(CovariantFinalizer::getToFinalizeTypeConstructor), this::typeConstructorToString)
-                            .withDetail("Finalized type constructor", templateClassWithMethods.getOptionalFinalizer().map(CovariantFinalizer::getFinalizedTypeConstructor), this::typeConstructorToString)
-                            .append(asNote());
-
-                    Map<TypeParameterName, TypeParameterName> classTypeParameterNameReplacements = findClassTypeParameterNameReplacements(templateClassWithMethods.getClassTypeParameters());
-                    TemplateClassWithMethods conflictFree = templateClassWithMethods.replaceTypeParameterNames(classTypeParameterNameReplacements);
-
-                    Log.of("Resolved (potential) conflicts between existing type parameters and new, generated type parameters")
-                            .withDetail("Class type parameters", conflictFree.getClassTypeParameters(), TypeParameterGenerator::generateFrom)
-                            .withDetail("Name of initializer method", conflictFree.getOptionalInitializer().map(CovariantInitializer::getName))
-                            .withDetail("Initialized type constructor", conflictFree.getOptionalInitializer().map(CovariantInitializer::getInitializedTypeConstructor), this::typeConstructorToString)
-                            .withDetail("Name of accumulator method", conflictFree.getAccumulator().getName())
-                            .withDetail("Input type constructor", conflictFree.getAccumulator().getInputTypeConstructor(), this::typeConstructorToString)
-                            .withDetail("Partially accumulated type constructor", conflictFree.getAccumulator().getPartiallyAccumulatedTypeConstructor(), this::typeConstructorToString)
-                            .withDetail("Accumulated type constructor", conflictFree.getAccumulator().getAccumulatedTypeConstructor(), this::typeConstructorToString)
-                            .withDetail("Name of finalizer method", conflictFree.getOptionalFinalizer().map(CovariantFinalizer::getName))
-                            .withDetail("To finalize type constructor", conflictFree.getOptionalFinalizer().map(CovariantFinalizer::getToFinalizeTypeConstructor), this::typeConstructorToString)
-                            .withDetail("Finalized type constructor", conflictFree.getOptionalFinalizer().map(CovariantFinalizer::getFinalizedTypeConstructor), this::typeConstructorToString)
-                            .append(asNote());
-
-                    String generated = generator()
-                            .withPackageName(containingClass.getPackageName())
-                            .withClassNameToGenerate(covariantBuilderAnnotation.className())
-                            .withClassTypeParameters(conflictFree.getClassTypeParameters())
-                            .withOptionalInitializer(conflictFree.getOptionalInitializer())
-                            .withAccumulator(conflictFree.getAccumulator())
-                            .withOptionalFinalizer(conflictFree.getOptionalFinalizer())
-                            .withParameterTypeConstructorArguments(PARAMETER_TYPE_CONSTRUCTOR_ARGUMENTS)
-                            .withReturnTypeConstructorArgument(RETURN_TYPE_CONSTRUCTOR_ARGUMENT)
-                            .withInputParameterNames(INPUT_PARAMETER_NAMES)
-                            .withValueParameterName(VALUE_PARAMETER_NAME)
-                            .withCombinatorParameterName(COMBINATOR_PARAMETER_NAME)
-                            .withLiftMethodName(covariantBuilderAnnotation.liftMethodName())
-                            .withMaxTupleSizeParameterName(MAX_TUPLE_SIZE_PARAMETER_NAME)
-                            .withMaxArity(covariantBuilderAnnotation.maxArity())
-                            .withSelfParameterName(SELF_PARAMETER_NAME)
-                            .withTupleParameterName(TUPLE_PARAMETER_NAME)
-                            .withElementParameterName(ELEMENT_PARAMETER_NAME)
-                            .generate();
-
-                    Log.of("Done generating code").append(asNote());
-
-                    FullyQualifiedName fullyQualifiedNameOfGeneratedClass = containingClass.getPackageName().withClassName(ClassName.of(covariantBuilderAnnotation.className()));
-                    try {
-                        JavaFileObject builderFile = processingEnv.getFiler().createSourceFile(containingClass.getPackageName().withClassName(ClassName.of(covariantBuilderAnnotation.className())).raw());
-                        try (PrintWriter out = new PrintWriter(builderFile.openWriter())) {
-                            out.print(generated);
-                            Log.of("Saved generated code to .java-file on disk (%s)", fullyQualifiedNameOfGeneratedClass.raw()).append(asNote());
-                        }
-                    } catch (IOException e) {
-                        Log.of("Error saving generated code to .java-file on disk (%s)", fullyQualifiedNameOfGeneratedClass.raw()).append(asError());
-                    }
+                    resolveConflictsAndGenerate(
+                            covariantBuilderAnnotation.className(),
+                            covariantBuilderAnnotation.liftMethodName(),
+                            covariantBuilderAnnotation.maxArity(),
+                            containingClass.getPackageName(),
+                            templateClassWithMethods
+                    );
                 } catch (Throwable t) {
                     Log.of("Error occurred while processing annotation of type '%s': %s", COVARIANT_BUILDER_CLASS, t.getMessage()).append(asError());
                     if (shouldLogNotes()) {
@@ -186,36 +118,6 @@ public class CovariantBuilderProcessor extends AbstractProcessor {
                 }
             });
         });
-
         return false;
-    }
-
-    private boolean shouldLogNotes() {
-        return Objects.equals(processingEnv.getOptions().getOrDefault(Options.VERBOSE_ARGUMENT, "false"), "true");
-    }
-
-    private LoggingBackend getMessagerLoggingBackend(Diagnostic.Kind diagnosticKind) {
-        return MessagerLoggingBackend.of(processingEnv, diagnosticKind);
-    }
-
-    private LoggingBackend asNote() {
-        return shouldLogNotes() ? getMessagerLoggingBackend(Diagnostic.Kind.NOTE) : NoLoggingBackend.INSTANCE;
-    }
-
-    private LoggingBackend asError() {
-        return getMessagerLoggingBackend(Diagnostic.Kind.ERROR);
-    }
-
-    private void printStackTraceToMessagerAsNote(Throwable e) {
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw);
-        e.printStackTrace(pw);
-        processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, sw.toString());
-    }
-
-    private String typeConstructorToString(TypeConstructor typeConstructor) {
-        Type substituteForPlaceholder = FullyQualifiedName.of("*").asType();
-        Type typeConstructorAsType = typeConstructor.apply(substituteForPlaceholder);
-        return TypeGenerator.generateFrom(typeConstructorAsType);
     }
 }
--- processor/src/main/java/nl/wernerdegroot/applicatives/processor/CovariantProcessor.java
+++ processor/src/main/java/nl/wernerdegroot/applicatives/processor/CovariantProcessor.java
@@ -3,17 +3,13 @@ package nl.wernerdegroot.applicatives.processor;
 import com.google.auto.service.AutoService;
 import nl.wernerdegroot.applicatives.processor.converters.ContainingClassConverter;
 import nl.wernerdegroot.applicatives.processor.converters.MethodConverter;
-import nl.wernerdegroot.applicatives.processor.domain.*;
+import nl.wernerdegroot.applicatives.processor.domain.FullyQualifiedName;
+import nl.wernerdegroot.applicatives.processor.domain.Method;
+import nl.wernerdegroot.applicatives.processor.domain.Modifier;
 import nl.wernerdegroot.applicatives.processor.domain.containing.ContainingClass;
-import nl.wernerdegroot.applicatives.processor.domain.type.Type;
-import nl.wernerdegroot.applicatives.processor.domain.typeconstructor.TypeConstructor;
 import nl.wernerdegroot.applicatives.processor.generator.ParameterGenerator;
 import nl.wernerdegroot.applicatives.processor.generator.TypeGenerator;
-import nl.wernerdegroot.applicatives.processor.generator.TypeParameterGenerator;
 import nl.wernerdegroot.applicatives.processor.logging.Log;
-import nl.wernerdegroot.applicatives.processor.logging.LoggingBackend;
-import nl.wernerdegroot.applicatives.processor.logging.MessagerLoggingBackend;
-import nl.wernerdegroot.applicatives.processor.logging.NoLoggingBackend;
 import nl.wernerdegroot.applicatives.processor.validation.TemplateClassWithMethods;
 import nl.wernerdegroot.applicatives.processor.validation.TemplateClassWithMethodsValidator;
 import nl.wernerdegroot.applicatives.processor.validation.Validated;
@@ -24,26 +20,16 @@ import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
-import javax.tools.Diagnostic;
-import javax.tools.JavaFileObject;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 
 import static nl.wernerdegroot.applicatives.processor.Classes.COVARIANT_CLASS;
 import static nl.wernerdegroot.applicatives.processor.Classes.COVARIANT_CLASS_NAME;
-import static nl.wernerdegroot.applicatives.processor.conflicts.ConflictFinder.findClassTypeParameterNameReplacements;
-import static nl.wernerdegroot.applicatives.processor.conflicts.Conflicts.*;
-import static nl.wernerdegroot.applicatives.processor.generator.Generator.generator;
 
 @SupportedOptions({Options.VERBOSE_ARGUMENT})
 @SupportedAnnotationTypes(COVARIANT_CLASS_NAME)
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 @AutoService(Processor.class)
-public class CovariantProcessor extends AbstractProcessor {
+public class CovariantProcessor extends AbstractCovariantProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
@@ -93,67 +79,13 @@ public class CovariantProcessor extends AbstractProcessor {
                     }
 
                     TemplateClassWithMethods templateClassWithMethods = validatedTemplateClassWithMethods.getValue();
-                    Log.of("All criteria for code generation satisfied")
-                            .withDetail("Class type parameters", templateClassWithMethods.getClassTypeParameters(), TypeParameterGenerator::generateFrom)
-                            .withDetail("Name of initializer method", templateClassWithMethods.getOptionalInitializer().map(CovariantInitializer::getName))
-                            .withDetail("Initialized type constructor", templateClassWithMethods.getOptionalInitializer().map(CovariantInitializer::getInitializedTypeConstructor), this::typeConstructorToString)
-                            .withDetail("Name of accumulator method", templateClassWithMethods.getAccumulator().getName())
-                            .withDetail("Input type constructor", templateClassWithMethods.getAccumulator().getInputTypeConstructor(), this::typeConstructorToString)
-                            .withDetail("Partially accumulated type constructor", templateClassWithMethods.getAccumulator().getPartiallyAccumulatedTypeConstructor(), this::typeConstructorToString)
-                            .withDetail("Accumulated type constructor", templateClassWithMethods.getAccumulator().getAccumulatedTypeConstructor(), this::typeConstructorToString)
-                            .withDetail("Name of finalizer method", templateClassWithMethods.getOptionalFinalizer().map(CovariantFinalizer::getName))
-                            .withDetail("To finalize type constructor", templateClassWithMethods.getOptionalFinalizer().map(CovariantFinalizer::getToFinalizeTypeConstructor), this::typeConstructorToString)
-                            .withDetail("Finalized type constructor", templateClassWithMethods.getOptionalFinalizer().map(CovariantFinalizer::getFinalizedTypeConstructor), this::typeConstructorToString)
-                            .append(asNote());
-
-                    Map<TypeParameterName, TypeParameterName> classTypeParameterNameReplacements = findClassTypeParameterNameReplacements(templateClassWithMethods.getClassTypeParameters());
-                    TemplateClassWithMethods conflictFree = templateClassWithMethods.replaceTypeParameterNames(classTypeParameterNameReplacements);
-
-                    Log.of("Resolved (potential) conflicts between existing type parameters and new, generated type parameters")
-                            .withDetail("Class type parameters", conflictFree.getClassTypeParameters(), TypeParameterGenerator::generateFrom)
-                            .withDetail("Name of initializer method", conflictFree.getOptionalInitializer().map(CovariantInitializer::getName))
-                            .withDetail("Initialized type constructor", conflictFree.getOptionalInitializer().map(CovariantInitializer::getInitializedTypeConstructor), this::typeConstructorToString)
-                            .withDetail("Name of accumulator method", conflictFree.getAccumulator().getName())
-                            .withDetail("Input type constructor", conflictFree.getAccumulator().getInputTypeConstructor(), this::typeConstructorToString)
-                            .withDetail("Partially accumulated type constructor", conflictFree.getAccumulator().getPartiallyAccumulatedTypeConstructor(), this::typeConstructorToString)
-                            .withDetail("Accumulated type constructor", conflictFree.getAccumulator().getAccumulatedTypeConstructor(), this::typeConstructorToString)
-                            .withDetail("Name of finalizer method", conflictFree.getOptionalFinalizer().map(CovariantFinalizer::getName))
-                            .withDetail("To finalize type constructor", conflictFree.getOptionalFinalizer().map(CovariantFinalizer::getToFinalizeTypeConstructor), this::typeConstructorToString)
-                            .withDetail("Finalized type constructor", conflictFree.getOptionalFinalizer().map(CovariantFinalizer::getFinalizedTypeConstructor), this::typeConstructorToString)
-                            .append(asNote());
-
-                    String generated = generator()
-                            .withPackageName(containingClass.getPackageName())
-                            .withClassNameToGenerate(covariantAnnotation.className())
-                            .withClassTypeParameters(conflictFree.getClassTypeParameters())
-                            .withOptionalInitializer(conflictFree.getOptionalInitializer())
-                            .withAccumulator(conflictFree.getAccumulator())
-                            .withOptionalFinalizer(conflictFree.getOptionalFinalizer())
-                            .withParameterTypeConstructorArguments(PARAMETER_TYPE_CONSTRUCTOR_ARGUMENTS)
-                            .withReturnTypeConstructorArgument(RETURN_TYPE_CONSTRUCTOR_ARGUMENT)
-                            .withInputParameterNames(INPUT_PARAMETER_NAMES)
-                            .withValueParameterName(VALUE_PARAMETER_NAME)
-                            .withCombinatorParameterName(COMBINATOR_PARAMETER_NAME)
-                            .withLiftMethodName(covariantAnnotation.liftMethodName())
-                            .withMaxTupleSizeParameterName(MAX_TUPLE_SIZE_PARAMETER_NAME)
-                            .withMaxArity(covariantAnnotation.maxArity())
-                            .withSelfParameterName(SELF_PARAMETER_NAME)
-                            .withTupleParameterName(TUPLE_PARAMETER_NAME)
-                            .withElementParameterName(ELEMENT_PARAMETER_NAME)
-                            .generate();
-
-                    Log.of("Done generating code").append(asNote());
-
-                    FullyQualifiedName fullyQualifiedNameOfGeneratedClass = containingClass.getPackageName().withClassName(ClassName.of(covariantAnnotation.className()));
-                    try {
-                        JavaFileObject builderFile = processingEnv.getFiler().createSourceFile(containingClass.getPackageName().withClassName(ClassName.of(covariantAnnotation.className())).raw());
-                        try (PrintWriter out = new PrintWriter(builderFile.openWriter())) {
-                            out.print(generated);
-                            Log.of("Saved generated code to .java-file on disk (%s)", fullyQualifiedNameOfGeneratedClass.raw()).append(asNote());
-                        }
-                    } catch (IOException e) {
-                        Log.of("Error saving generated code to .java-file on disk (%s)", fullyQualifiedNameOfGeneratedClass.raw()).append(asError());
-                    }
+                    resolveConflictsAndGenerate(
+                            covariantAnnotation.className(),
+                            covariantAnnotation.liftMethodName(),
+                            covariantAnnotation.maxArity(),
+                            containingClass.getPackageName(),
+                            templateClassWithMethods
+                    );
                 } catch (Throwable t) {
                     Log.of("Error occurred while processing annotation of type '%s': %s", COVARIANT_CLASS, t.getMessage()).append(asError());
                     if (shouldLogNotes()) {
@@ -165,34 +97,5 @@ public class CovariantProcessor extends AbstractProcessor {
             });
         });
         return true;
-    }
-
-    private void printStackTraceToMessagerAsNote(Throwable e) {
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw);
-        e.printStackTrace(pw);
-        processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, sw.toString());
-    }
-
-    private String typeConstructorToString(TypeConstructor typeConstructor) {
-        Type substituteForPlaceholder = FullyQualifiedName.of("*").asType();
-        Type typeConstructorAsType = typeConstructor.apply(substituteForPlaceholder);
-        return TypeGenerator.generateFrom(typeConstructorAsType);
-    }
-
-    private boolean shouldLogNotes() {
-        return Objects.equals(processingEnv.getOptions().getOrDefault(Options.VERBOSE_ARGUMENT, "false"), "true");
-    }
-
-    private LoggingBackend getMessagerLoggingBackend(Diagnostic.Kind diagnosticKind) {
-        return MessagerLoggingBackend.of(processingEnv, diagnosticKind);
-    }
-
-    private LoggingBackend asNote() {
-        return shouldLogNotes() ? getMessagerLoggingBackend(Diagnostic.Kind.NOTE) : NoLoggingBackend.INSTANCE;
-    }
-
-    private LoggingBackend asError() {
-        return getMessagerLoggingBackend(Diagnostic.Kind.ERROR);
     }
 }
